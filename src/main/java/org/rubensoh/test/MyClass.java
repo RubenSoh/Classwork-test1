bde3d39b5382e0c2c4e3456bad00dd7f5d794eff
Ruben Soh <sohruben.mailinglists@gmail.com>
2021-11-22 09:32:05 -0500

    make hello goodbye instead

--- src/main/java/org/rubensoh/test/MyClass.java
+++ src/main/java/org/rubensoh/test/MyClass.java
@@ -3,7 +3,7 @@ package org.rubensoh.test;
 public class MyClass {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Goodbye world");
     }
 
 }
